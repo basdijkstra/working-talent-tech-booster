06b15204f199ae19c45c3ecf9b337afe6841ae27
Bas Dijkstra <bas@ontestautomation.com>
2023-07-10 16:51:08 +0200

    Add exercises for integration tests

---
+++ WorkingTalentTechBooster/IntegrationTestBase.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using WireMock.Matchers;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace WorkingTalentTechBooster
+{
+    public class IntegrationTestBase
+    {
+        private WireMockServer? server;
+
+        private string loanRequest_100_10 = "{\"Amount\":100,\"DownPayment\":10}";
+        private string loanRequest_500_50 = "{\"Amount\":500,\"DownPayment\":50}";
+        private string loanRequest_500_10 = "{\"Amount\":500,\"DownPayment\":10}";
+
+        private string responseApproved = "{\"Result\":\"Approved\"}";
+        private string responseDenied = "{\"Result\":\"Denied\"}";
+
+
+        [SetUp]
+        public void StartMockServer()
+        {
+            server = WireMockServer.Start(9876);
+
+            CreateStubFor10010LoanRequest();
+            CreateStubFor50050LoanRequest();
+            CreateStubFor50010LoanRequest();
+        }
+
+        [TearDown]
+        public void StopMockServer()
+        {
+            server?.Stop();
+        }
+
+        private void CreateStubFor10010LoanRequest()
+        {
+            server?.Given(Request.Create().WithPath("/requestLoan").UsingPost()
+                .WithBody(new JsonMatcher(loanRequest_100_10)))
+                .RespondWith(Response.Create()
+                .WithStatusCode(201)
+                .WithHeader("Content-Type", "application/json")
+                .WithBody(responseApproved));
+        }
+        private void CreateStubFor50050LoanRequest()
+        {
+            server?.Given(Request.Create().WithPath("/requestLoan").UsingPost()
+                .WithBody(new JsonMatcher(loanRequest_500_50)))
+                .RespondWith(Response.Create()
+                .WithStatusCode(201)
+                .WithHeader("Content-Type", "application/json")
+                .WithBody(responseApproved));
+        }
+        private void CreateStubFor50010LoanRequest()
+        {
+            server?.Given(Request.Create().WithPath("/requestLoan").UsingPost()
+                .WithBody(new JsonMatcher(loanRequest_500_10)))
+                .RespondWith(Response.Create()
+                .WithStatusCode(201)
+                .WithHeader("Content-Type", "application/json")
+                .WithBody(responseDenied));
+        }
+    }
+}
---
+++ WorkingTalentTechBooster/ParaBankIntegrationTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+using static RestAssured.Dsl;
+
+namespace WorkingTalentTechBooster
+{
+    [TestFixture]
+    public class ParaBankIntegrationTests : IntegrationTestBase
+    {
+        [Test]
+        public void RequestLoanFor100With10AsDownPayment_ShouldBeAccepted()
+        {
+            var loanRequest = new
+            {
+                Amount = 100,
+                DownPayment = 10
+            };
+
+            Given()
+                .Body(loanRequest)
+                .When()
+                .Post("http://localhost:9876/requestLoan")
+                .Then()
+                .StatusCode(201)
+                .And()
+                .Body("Result", NHamcrest.Is.EqualTo("Approved"));
+        }
+
+        [Test]
+        public void RequestLoanFor500With50AsDownPayment_ShouldBeAccepted()
+        {
+            // Create a new loanRequest with Amount = 500 and DownPayment = 50
+
+            // POST that to the same endpoint
+
+            // Check that the Result is equal to Approved
+        }
+
+        [Test]
+        public void RequestLoanFor500With10AsDownPayment_ShouldBeDenied()
+        {
+            // Create a new loanRequest with Amount = 500 and DownPayment = 10
+
+            // POST that to the same endpoint
+
+            // Check that the Result is equal to Denied
+        }
+
+        [Test]
+        public void IsntThereABetterWay()
+        {
+            // The tests above perform the same action (submitting a loan request, then
+            // checking the result), just with different data values (arguments).
+
+            // That results in a lot of code duplication
+
+            // Isn't there a better way?
+
+            // Hint: https://www.ontestautomation.com/parameterizing-tests-in-rest-assured-net/
+        }
+    }
+}
