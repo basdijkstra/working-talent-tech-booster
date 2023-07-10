93cef7d1bf11774eaf8ee62dbc7b645e5311e0c0
Bas Dijkstra <bas@ontestautomation.com>
2023-07-10 16:55:34 +0200

    Add test to check installation

--- WorkingTalentTechBooster/ParaBankFullStackTests.cs
+++ WorkingTalentTechBooster/ParaBankFullStackTests.cs
@@ -1,13 +1,32 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace WorkingTalentTechBooster
 {
     public class ParaBankFullStackTests
     {
+        private WebDriver? driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
         [Test]
         public void LetsSeeIfEverythingWorks()
         {
+            driver?.Navigate().GoToUrl("https://parabank.parasoft.com");
 
+            Assert.That(driver?.Title, Is.EqualTo("ParaBank | Welcome | Online Banking"));
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver?.Quit();
         }
     }
 }
